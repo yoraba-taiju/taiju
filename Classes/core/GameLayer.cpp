bdfbc92a8d81fd9ef18f9cfcc35e60f0fd32c635
PSI <ledyba@users.sourceforge.jp>
2015-06-19 05:30:24 +0900

    move listeners to onEnter/Exit

--- Classes/core/GameLayer.cpp
+++ Classes/core/GameLayer.cpp
@@ -17,6 +17,11 @@ bool GameLayer::init(cocos2d::Size fieldSize) {
 	this->setContentSize(fieldSize);
 	player->setPosition(fieldSize/2);
 	
+	return true;
+}
+
+void GameLayer::onEnter() {
+	this->LayerColor::onEnter();
 	auto listener = EventListenerTouchOneByOne::create();
 	listener->setSwallowTouches(true);
 	
@@ -26,10 +31,18 @@ bool GameLayer::init(cocos2d::Size fieldSize) {
 	
 	auto disp = Director::getInstance()->getEventDispatcher();
 	disp->addEventListenerWithSceneGraphPriority(listener, this);
-
-	return true;
+	this->touchListener_ = listener;
+}
+void GameLayer::onExit() {
+	this->LayerColor::onExit();
+	auto disp = Director::getInstance()->getEventDispatcher();
+	disp->removeEventListener(this->touchListener_);
+	this->touchListener_ = nullptr;
 }
 
+
+void GameLayer::onGameover(){
+}
 
 bool GameLayer::onTouchBegan(cocos2d::Touch* touch,cocos2d::Event* event) {
 	return true;
--- Classes/core/GameLayer.h
+++ Classes/core/GameLayer.h
@@ -6,9 +6,14 @@
 class GameLayer : public cocos2d::LayerColor {
 private:
 	static const constexpr int TAG_PLAYER=1;
+private:
+	cocos2d::EventListener* touchListener_;
 public:
 	GameLayer();
 	bool init(cocos2d::Size screenSize);
+	virtual void onEnter() override;
+	virtual void onExit() override;
+	void onGameover();
 	bool onTouchBegan(cocos2d::Touch* touch,cocos2d::Event* event);
 	void onTouchMoved(cocos2d::Touch* touch, cocos2d::Event* event);
 	void onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* event);
