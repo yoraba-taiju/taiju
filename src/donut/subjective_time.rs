e4791913725cc19d00f62bb930c15e4fd89acd16
Kaede Fujisaki <kaede@hexe.net>
2021-05-05 13:18:19 +0900

    Supresss warnings

--- src/donut/subjective_time.rs
+++ src/donut/subjective_time.rs
@@ -1,13 +1,12 @@
-use super::*;
-
 #[derive(Ord, PartialOrd, Eq, PartialEq, Debug, Clone, Copy)]
 pub struct SubjectiveTime {
-  pub(crate) leaps: u32,
-  pub(crate) ticks: u32,
+  pub leaps: u32,
+  pub ticks: u32,
 }
 
 impl SubjectiveTime {
-  pub(crate) fn new(leaps:u32, ticks:u32) -> Self {
+  #[allow(dead_code)]
+  pub fn new(leaps:u32, ticks:u32) -> Self {
     Self {
       leaps,
       ticks,
