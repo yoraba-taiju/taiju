b4b605e229cfafa9d9ce918b25f4b28b86c0c334
Kaede Fujisaki <kaede@hexe.net>
2021-05-18 09:50:10 +0900

    add TODO

--- src/loading/chapter.rs
+++ src/loading/chapter.rs
@@ -13,8 +13,11 @@ pub fn on_enter<'a, 'b, 'c>(
   asset_server: Res<'b, AssetServer>,
   color_materials: ResMut<'c, Assets<ColorMaterial>>,
 ) {
+  // TODO (ledyba):
   // なぜかここで一回moveしておくと使えるようになる。
-  // 関数の引数の生存区間がなんかおかしい気がする
+  // 関数の引数の生存区間解析がなんかおかしい気がする
+  // これだとしたら、NLLが有効になったら直るはず。
+  // https://github.com/rust-lang/rust/issues/74256
   let asset_server = asset_server;
   let mut color_materials = color_materials;
   let mut loader = Loader::new(&asset_server, &mut color_materials);
