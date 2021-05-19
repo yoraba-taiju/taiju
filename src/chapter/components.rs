52bb3038e40ee9b623d092235aa34ef7cac28470
Kaede Fujisaki <kaede@hexe.net>
2021-05-20 03:55:00 +0900

    witch bullet

--- src/chapter/components.rs
+++ src/chapter/components.rs
@@ -5,10 +5,10 @@ pub mod lifecycle;
 pub mod geometry;
 
 pub mod witch;
+pub mod witch_bullet;
 
 pub mod enemy;
 pub mod enemy_bullet;
 
 pub mod scape;
 pub mod background;
-
---
+++ src/chapter/components/witch_bullet.rs
@@ -0,0 +1,6 @@
+use crate::prelude::*;
+
+pub struct WitchBulet;
+
+pub struct SoraBullet;
+
