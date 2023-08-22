9356f20b995d449d1ac4fefbdf0a14258b625beb
Kaede Fujisaki <kaede@hexe.net>
2023-08-22 22:40:01 +0900

    Add some system for testing

--- taiju/src/lib.rs
+++ taiju/src/lib.rs
@@ -1,7 +1,29 @@
 use bevy::prelude::*;
 
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+enum GameState {
+  #[default]
+  Menu,
+  InGame,
+}
+
 pub fn build_app() -> App {
   let mut app = App::new();
   app.add_plugins(DefaultPlugins);
+  app.add_state::<GameState>();
+  app.add_systems(PreUpdate, sys.run_if(in_state(GameState::Menu)));
   app
+}
+
+fn sys(mut stat: ResMut<State<GameState>>) {
+  let current = *stat.get();
+  *stat = match current {
+    GameState::InGame => State::new(GameState::Menu),
+    GameState::Menu => State::new(GameState::InGame),
+  };
+  println!("{:?}", stat.get());
+}
+
+fn trans() {
+  println!("Trans!")
 }
